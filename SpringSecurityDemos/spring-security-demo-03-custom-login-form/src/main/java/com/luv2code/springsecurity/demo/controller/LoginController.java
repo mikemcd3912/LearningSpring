5143f9c40a1f857cb7913db44500c93141fcdc47
Mike McDonald <mike.mcdonald3912@gmail.com>
2021-11-03 17:53:50 -0700

    Spring Security Work - 11.3.2021
    Spring Security, Login Logout and Cross Site Requests

---
+++ SpringSecurityDemos/spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		return "plain-login";
+	}
+	
+//	@PostMapping("/authenticateTheUser")
+//	public String authenticateTheUser() {
+//		return 
+//	}
+}
